905597907ca371e82bf562b125cd3d1999531597
Nur Fajar S A <nur.fajar11@ui.ac.id>
2022-03-11 00:17:03 +0700

    Optimalize

--- lib/validate.dart
+++ lib/validate.dart
@@ -1,57 +1,47 @@
-  String? isEmpty(value) {
-    if (value.isEmpty) {
-      return "Please complete this field";
-    } else {
-      return null;
-    }
+String? isEmpty(value) {
+  if (value.isEmpty) {
+    return "Please complete this field";
+  } else {
+    return null;
   }
+}
 
-  String? isValidDay(value) {
-    if (value.isEmpty) {
-      return "";
-    } else if ((0 == int.parse(value)) || (int.parse(value) > 31)) {
-      return "";
-    } else {
-      return null;
-    }
+String? isValidDay(value) {
+  if (value.isEmpty || (0 == int.parse(value)) || (int.parse(value) > 31)) {
+    return "";
+  } else {
+    return null;
   }
+}
 
-  String? isValidMonth(value) {
-    if (value.isEmpty) {
-      return "";
-    } else if ((0 == int.parse(value)) || (int.parse(value) > 12)) {
-      return "";
-    } else {
-      return null;
-    }
+String? isValidMonth(value) {
+  if (value.isEmpty || (0 == int.parse(value)) || (int.parse(value) > 12)) {
+    return "";
+  } else {
+    return null;
   }
+}
 
-  String? isValidYear(value) {
-    if (value.isEmpty) {
-      return "";
-    } else if (value.length != 4) {
-      return "";
-    } else {
-      return null;
-    }
+String? isValidYear(value) {
+  if (value.isEmpty || value.length != 4) {
+    return "";
+  } else {
+    return null;
   }
+}
 
-  String? isValidHour(value){
-    if (value.isEmpty) {
-      return "";
-    } else if (int.parse(value)>24) {
-      return "";
-    } else {
-      return null;
-    }
+String? isValidHour(value) {
+  if (value.isEmpty || int.parse(value) > 24 ||value.length != 2) {
+    return "";
+  } else {
+    return null;
   }
+}
 
-  String? isValidMin(value){
-    if (value.isEmpty) {
-      return "";
-    } else if (int.parse(value)>59) {
-      return "";
-    } else {
-      return null;
-    }
+String? isValidMin(value) {
+  if (value.isEmpty || int.parse(value) > 59 || value.length != 2) {
+    return "";
+  } else {
+    return null;
   }
+}
